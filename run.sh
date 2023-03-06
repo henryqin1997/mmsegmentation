af0a6968d0663ddea0a183a64805925db43994a2
henryqin1997 <henryqin1997@gamil.com>
2023-03-06 23:02:39 +0800

    replace train dataset and dataloader with customized version

---
+++ run.sh
@@ -0,0 +1,3 @@
+(CUDA_VISIBLE_DEVICES=0,1,2,3 tools/dist_train.sh configs/upernet/upernet_r50_512x512_60k_ade20k.py 4 &
+CUDA_VISIBLE_DEVICES=4,5,6,7 tools/dist_train.sh configs/upernet/upernet_r50_512x512_56k_ade20k.py 4 );
+python3 /mnt/bn/dc-in-nas/zhanka.py
